844230f530418ceac9054fd4ea82960b56289305
Jordi272 <jordidomenech27@gmail.com>
2022-03-18 16:57:55 +0100

    Algoritmo recursivo y errores solucionados

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 namespace Torres_de_Hanoi
 {
     class Hanoi
-    {
-        /*TODO: Implementar métodos*/
+    {      
         public void mover_disco(Pila a, Pila b)
         {          
             if (a.isEmpty() && b.Top != 0)
@@ -46,7 +45,7 @@ namespace Torres_de_Hanoi
 
 
         }
-        public int movimientos = 0;
+        
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
@@ -115,9 +114,30 @@ namespace Torres_de_Hanoi
             }
             return incrementar_movimientos();
         }
-        
-       
 
+        public int contador = 0;
+        public int recursivo(int n, Pila ini, Pila fin, Pila aux)
+        {
+            
+
+            if (n == 1)
+            {
+                mover_disco(ini, fin);
+                contador++;
+            }
+            else
+            {
+
+                recursivo(n - 1, ini, aux, fin);
+                mover_disco(ini, fin);
+                contador++;
+                recursivo(n - 1, aux, fin, ini);
+
+            }
+            return contador;
+        }
+
+        public int movimientos = 0;
         public int incrementar_movimientos()
         {
             return movimientos++;
--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -11,7 +11,7 @@ namespace Torres_de_Hanoi
         static void Main(string[] args)
         {
             
-            Hanoi objetoHanoi = new Hanoi();
+            Hanoi Hanoi = new Hanoi();
 
             Pila Ini = new Pila();
             Pila Aux = new Pila();
@@ -28,26 +28,44 @@ namespace Torres_de_Hanoi
             Ini.push(d1);
 
 
-            String numero;
-            numero = Console.ReadLine();
-            if (Int32.Parse(numero) == 1)
+            Pila Inir = new Pila();
+            Pila Auxr = new Pila();
+            Pila Finalr = new Pila();
+
+ 
+            Disco d1r = new Disco(1);
+            Disco d2r = new Disco(2);
+            Disco d3r = new Disco(3);
+            Disco d4r = new Disco(4);
+
+            Inir.push(d4r);
+            Inir.push(d3r);
+            Inir.push(d2r);
+            Inir.push(d1r);
+
+            Console.WriteLine("Seleccione un algoritmo:" + Environment.NewLine);
+            Console.WriteLine("1 para iterativo");
+            Console.WriteLine("Otro número para recursivo");
+
+            String numeroIntroducido;
+            numeroIntroducido = Console.ReadLine();
+            if (Int32.Parse(numeroIntroducido) == 1)
             {
-                Console.WriteLine("----------------------");
-                Console.WriteLine("MOVIMIENTOS ITERATIVOS");
-                Console.WriteLine("----------------------");
+                Console.WriteLine("ALGORITMO ITERARITVO");
 
-                int iterativoResultado = objetoHanoi.iterativo(4, Ini, Final, Aux);
-                Console.WriteLine("RESULTADO ITERATIVO --> " + iterativoResultado);
-
-             
+                int iterativo = Hanoi.iterativo(4,Ini, Final, Aux);
+                Console.WriteLine("El núemro de moviemientos es: " + iterativo);
+              
             }
             else
             {
-                Console.WriteLine("Pulse 1");
+                Console.WriteLine("ALGORITMO RECURSIVO");
+
+                int recursivoResultado = Hanoi.recursivo(4, Inir, Finalr, Auxr);
+                Console.WriteLine("El núemro de moviemientos es: " + recursivoResultado);
+             
             }
             Console.ReadKey();
-
-
         }
     }
 }
